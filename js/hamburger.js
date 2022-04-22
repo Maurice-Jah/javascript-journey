d876eeb891d9f7d545b78d16955e6defa66c1361
Maurice-Jah <mauricejahwizwid@gmail.com>
2022-04-22 02:53:14 +0100

    create hamburger

---
+++ js/hamburger.js
@@ -0,0 +1,5 @@
+let menu = document.getElementById("menu");
+menu.addEventListener("click", () => menu.classList.toggle("openMenu"));
+
+let menu2 = document.getElementById("menu2");
+menu2.addEventListener("click", () => menu2.classList.toggle("openMenu2"));
